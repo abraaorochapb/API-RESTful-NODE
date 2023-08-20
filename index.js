da79177c7d2656c49ac986e8063be447647d379b
abraaorochapb <abraaorochapb@gmail.com>
2023-08-20 11:12:28 -0300

    Conexão com o banco de dados MongoDB atlas via mongoose

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  console.log('http://localhost:3000');
+mongoose.connect('mongodb+srv://abraaorochapb:Abra&120@cluster0.d3rgnx0.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Connected to MongoDB')
+  app.listen(3000)
+})
+.catch((err) => {
+  console.log(err)
 })
 
 
--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
-module.exports = routes
-
 routes.get('/', (req, res) => {
-  res.send('Server is running!')
+  res.json({message : 'Server is running!'})
 })
+
+module.exports = routes
