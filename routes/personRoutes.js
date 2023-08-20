1161d6e6a2bf7978416b984af029314dcc6d631d
abraaorochapb <abraaorochapb@gmail.com>
2023-08-20 19:09:07 -0300

    Criação das rotas de Read e Update

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const Person = require('../models/Person');
 
+// Create 
 router.post('/', async (req, res) => {
   const { name, salary, approved } = req.body;
 
   const person = { name, salary, approved };
+
+  if (!name) {
+    res.status(400).json({ error: 'Missing name, salary or approved' });
+    return
+  }
 
   try {
     await Person.create(person);
@@ -14,5 +20,56 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Error creating person' });
   }
 });
+
+// Read
+router.get('/', async (req, res) => {
+  try {
+    const people = await Person.find();
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ error: 'Error getting people' });
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const person = await Person.findById(id);
+
+    if (!person) {
+      res.status(400).json({ error: 'invalid id' });
+      return
+    }
+
+    res.status(200).json(person);
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Error getting person' });
+  }
+})
+
+// Update (PUT, PATCH)
+
+router.patch('/:id', async (req, res) => {
+  const  { id }  = req.params;
+  const { name, salary, approved } = req.body;
+
+  const person = { name, salary, approved };
+
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(id, person);
+
+    if (updatedPerson.matchedCount === 0) {
+      res.status(422).json({ error: 'User not found' });
+      return
+    }
+
+    res.status(200).json({ message: 'Person updated!' });
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Error updating person' });
+  }
+})
 
 module.exports = router
