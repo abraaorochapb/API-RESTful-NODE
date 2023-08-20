f76b98c20dd54dac0af6802ffea3cb9b9ffed2d0
abraaorochapb <abraaorochapb@gmail.com>
2023-08-20 18:08:40 -0300

    Adicionada rota de criação de novos usuarios

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes');
 const mongoose = require('mongoose');
+const Person = require('./models/Person');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+
+
+app.get('/', (req, res) => {
+  res.json({message : 'Server is running!'})
+})
+
+app.post('/person', async (req, res) => {
+  const { name, salary, approved } = req.body;
+
+  const person = { name, salary, approved };
+
+  try {
+    await Person.create(person);
+
+    res.status(201).json({ message: 'Person created!' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating person' });
+  }
+});
+
 
 mongoose.connect('mongodb+srv://abraaorochapb:Abra&120@cluster0.d3rgnx0.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
@@ -15,6 +34,3 @@ mongoose.connect('mongodb+srv://abraaorochapb:Abra&120@cluster0.d3rgnx0.mongodb.
 .catch((err) => {
   console.log(err)
 })
-
-
-
--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-routes.get('/', (req, res) => {
-  res.json({message : 'Server is running!'})
-})
-
-module.exports = routes
